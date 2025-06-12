930683484ddc7ec201c3de54ce0ac633fafb4c16
Patricia Cataño <catanop347@gmail.com>
2025-06-12 11:32:02 -0400

    Configuracion del JWT en el frontend

--- frontend_pizzeria/src/App.vue
+++ frontend_pizzeria/src/App.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import { useAuthStore } from "@/stores/index";
+const authStore = useAuthStore();
 </script>
 
 <template>
@@ -8,15 +10,18 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
+       <HelloWorld :msg="'SIS257' + (authStore.user ? ' - Hola ' + authStore.user : '')" />
       <nav>
+	<RouterLink to="/">Inicio</RouterLink>
+	<RouterLink v-if="!authStore.token" to="/login" >Iniciar Sesión</RouterLink>
+	<span v-else>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
         <RouterLink to="/producto">Productos</RouterLink>
         <RouterLink to="/cliente">Clientes</RouterLink>
-
-      </nav>
+	  <a @click="authStore.logout()" class="text-danger">Salir</a>
+	</span>
+  </nav>
     </div>
   </header>
 
--- frontend_pizzeria/src/router/index.ts
+++ frontend_pizzeria/src/router/index.ts
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import { useAuthStore } from '@/stores';
+import { getTokenFromLocalStorage } from '@/helpers';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -27,7 +29,23 @@ const router = createRouter({
       name: 'cliente',
       component: () => import('../views/ClienteView.vue'),
     },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('../views/LoginView.vue')
+    }
   ],
 })
 
 export default router
+router.beforeEach(async to => {
+  const publicPages = ["/login"];
+  const authRequired = !publicPages.includes(to.path);
+  const authStore = useAuthStore();
+
+  if (authRequired && !getTokenFromLocalStorage()) {
+    if (authStore) authStore.logout();
+    authStore.returnUrl = to.fullPath;
+    return "/login";
+  }
+});
